43bfad37cb9a9dc805bf6823c43b9c2ab906e233
youba idjis <youba.etud@gmail.com>
2023-02-17 15:02:31 -0800

    adding tires

--- test/test_battery/test_spindler_battery.py
+++ test/test_battery/test_spindler_battery.py
@@ -7,7 +7,7 @@ from battery.spindler_battery import SpindlerBattery
 class TestSpindlerBattery(unittest.TestCase):
     def test_needs_service_true(self):
         current_date = date.fromisoformat("2020-05-15")
-        last_service_date = date.fromisoformat("2018-01-25")
+        last_service_date = date.fromisoformat("2017-01-25")
         battery = SpindlerBattery(current_date, last_service_date)
         self.assertTrue(battery.needs_service())
 
